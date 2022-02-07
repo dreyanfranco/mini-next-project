dab1fd5775f45ba1a8dba20ed3966cb70db44839
dreyanfranco <dreyanfranco@gmail.com>
2022-02-07 22:09:25 +0100

    next.js project before disabling eslint

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const about = () => {
+    return (
+        <div>
+            <head>
+                <title>About</title>
+            </head>
+            <h1>About</h1>
+        </div>
+    );
+};
+
+export default about
